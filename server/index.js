ba8ba26c1a3f7f0f9b0d3e527e19f540d1209265
Anne-Sophie SIMON <a-sophie.simon@live.fr>
2023-08-21 15:53:04 -0400

    add schema

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 //Connect to database
 connectDB();
+
+app.use(cors());
+
 
 app.use(
     '/graphql', 
